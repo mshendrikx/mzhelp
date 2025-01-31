0e111840252187229e0c4d2344b07e9dc7062f54
Maurício Servatius Hendrikx <mauricio.servatius@gmail.com>
2025-01-31 18:36:19 -0300

    main

--- job_transfer.py
+++ job_transfer.py
@@ -6,7 +6,7 @@ from seleniumbase import SB
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-from project.models import Player, PlayerTraining, User
+from project.models import Player, PlayerTraining, Countries, Tranfers
 from project.common import (
     get_db,
     only_numerics,
@@ -23,37 +23,70 @@ load_dotenv()
 
 session = get_db()
 
-users = session.query(User).filter(User.id > 1).all()
-
 utc_string = get_utc_string(format="%Y-%m-%d")
-mz_day = get_mz_day(date=utc_string)
 
-for user in users:
+def get_transfer_searches(countries):
+    
+    searches = []
+    for country in countries:        
+        if country.ages > 18:
+            age = 19
+            while age <= country.ages:
+                search = []
+                search.append(str(country.id))
+                search.append(str(age))
+                search.append(str(age))   
+                age += 1
+                searches.append(search)
+            search = []
+            search.append(str(country.id))
+            search.append(str(age))
+            search.append('37')
+            searches.append(search)                          
+        else:
+            search = []
+            search.append(str(country.id))
+            search.append('19')
+            search.append('37')
+            searches.append(search)
+            
+    return searches
 
-    with SB(uc=True) as sb:
-
-        sb.open("https://www.managerzone.com/")
-        sb.click('button[id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]')
-        sb.type('input[id="login_username"]', user.mzuser)
-        sb.type('input[id="login_password"]', user.mzpass)
-        sb.click('a[id="login"]')
-        sb.open("https://www.managerzone.com/?p=team")
-        text = sb.get_text('//*[@id="infoAboutTeam"]/dd[1]/span[3]')
-        teamid = int(only_numerics(text))
-
-        players = session.query(Player).filter_by(teamid=teamid)
-
-        for player in players:
-            player.teamid = 0
-
-        session.commit()
-
-        sb.open("https://www.managerzone.com/?p=players")
-
-        players_container = sb.find_element("#players_container")
-        soup = BeautifulSoup(players_container.get_attribute("outerHTML"), "lxml")
-        players_soup = soup.find_all(class_="playerContainer")
-
+with SB(uc=True) as sb:
+    sb.open("https://www.managerzone.com/")
+    sb.click('button[id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]')
+    sb.type('input[id="login_username"]', os.environ.get("MZUSER"))
+    sb.type('input[id="login_password"]', os.environ.get("MZPASS"))
+    sb.click('a[id="login"]')
+    sb.open("https://www.managerzone.com/?p=transfer")
+    sb.wait_for_element('//*[@id="players_container"]')
+    utc_string = get_utc_string(format="%Y-%m-%d")
+    
+    searches = get_transfer_searches(session.query(Countries).all())
+    pages_soup = []
+    for search in searches:
+        sb.click_xpath('//*[@id="resetb"]')
+        sb.select_option_by_value('//*[@id="deadline"]', '3')
+        sb.select_option_by_value('//*[@id="nationality"]', search[0]) 
+        sb.type('input[id="agea"]', search[1])
+        sb.type('input[id="ageb"]', search[2])        
+        sb.click_xpath('//*[@id="searchb"]')
+        next_link = True
+        while next_link:
+            sb.wait_for_element('//*[@id="players_container"]')
+            players_container = sb.find_element("#players_container")
+            soup = BeautifulSoup(players_container.get_attribute("outerHTML"), "lxml")
+            pages_soup.append(soup)
+            try:
+                button_next = sb.find_element("div.transferSearchPages a:contains('Next')", timeout=2)
+                button_next.click()
+            except:
+                next_link = False
+            break
+        break
+    
+    for page_soup in pages_soup:
+        players_soup = page_soup.find_all(class_="playerContainer")
         players = []
         players_training = []
         countries = countries_data(index=1)
@@ -69,19 +102,21 @@ for user in users:
                 session.add(player)
                 session.commit()               
             player.date = utc_string
-            player.teamid = teamid                      
             player.name = header.find(class_="player_name").text
-            container = player_soup.div.div
-            playerview_info = container.find(class_="dg_playerview_info")
-            player_info = playerview_info.find("tbody")
-            player_chars = player_info.find_all("tr")
-            scout_report = playerview_info.find(title="Scout report")
+            float_left = player_soup.find(class_="floatLeft")
+            float_left = float_left.table.tbody
+            float_left = float_left.find_all("tr")
+            player_chars = float_left[0].find_all("tr")
+            player_skills = float_left[8].find_all("tr")
+            float_right = player_soup.find(class_="floatRight transfer-control-area")
+            float_right = float_right.find_all(class_="box_dark")
+            scout_report = float_right[1].find(title="Scout report")
             if scout_report == None:
                 player.starhigh = 0
                 player.starlow = 0
                 player.startraining = 0
-            training_graph = playerview_info.find(
-                class_="player_icon_placeholder training_graphs soccer"
+            training_graph = float_right[1].find(
+                class_="fa-regular fa-chart-line-up training-graphs-icon"
             )
             if training_graph != None:
                 player_training = session.query(PlayerTraining).filter_by(id=player.id).first()
@@ -93,159 +128,78 @@ for user in users:
             player.salary = 0
             for player_char in player_chars:
                 if "Age" in player_char.text:
-                    tds = player_char.find_all("td")
-                    player.age = int(only_numerics(tds[0].text))
+                    player.age = int(only_numerics(player_char.text))
                     player.transferage = player.age
-                    if "Left" in tds[1].text:
+                elif "Foot" in player_char.text:
+                    if "Left" in player_char.text:
                         player.foot = 0
-                    elif "Right" in tds[1].text:
+                    elif "Right" in player_char.text:
                         player.foot = 1
                     else:
                         player.foot = 2
                 elif "Height" in player_char.text:
-                    tds = player_char.find_all("td")
-                    player.height = int(only_numerics(tds[0].text))
-                    player.weight = int(only_numerics(tds[1].text))
-                elif "Birth" in player_char.text:
-                    player.season = int(only_numerics(player_char.td.text))
-                elif "Nationality" in player_char.text:
-                    player.country = countries[player_char.td.span.text].id
-                    if len(player_char.find_all("a")) == 0:
-                        player.national = 0
-                    else:
-                        player.national = 1
-                elif "Value" in player_char.text:
-                    player.value = int(only_numerics(player_char.td.text))
-                elif "Salary" in player_char.text:
-                    player.salary = int(only_numerics(player_char.td.text))
+                    player.height = int(only_numerics(player_char.text))
+                elif "Weight" in player_char.text:
+                    player.weight = int(only_numerics(player_char.text))
+                elif "Born" in player_char.text:
+                    player.season = int(only_numerics(player_char.text))
                 elif "Balls" in player_char.text:
-                    player.totalskill = int(only_numerics(player_char.td.text))
-            player_skills = container.find(class_="skills-container floatLeft clearfix")
-            player_skills = player_skills.find("tbody")
-            player_skills = player_skills.find_all("tr")
+                    player.totalskill = int(only_numerics(player_char.text))
+            
+            money_info = float_right[0].find_all("span")
+            player.value = int(only_numerics(money_info[0].text))
+            player.salary = int(only_numerics(money_info[1].text))
+            
             count = 0
             for player_skill in player_skills:
                 match count:
                     case 0:
                         player.speedscout = 0
                         player.speed = int(only_numerics(player_skill.text))
-                        maxed = player_skill.find(class_="maxed")
-                        if maxed:
-                            player.speedmax = 1
-                        else:
-                            player.speedmax = 0
                     case 1:
                         player.staminascout = 0
                         player.stamina = int(only_numerics(player_skill.text))
-                        maxed = player_skill.find(class_="maxed")
-                        if maxed:
-                            player.staminamax = 1
-                        else:
-                            player.staminamax = 0
                     case 2:
                         player.intelligencescout = 0
                         player.intelligence = int(only_numerics(player_skill.text))
-                        maxed = player_skill.find(class_="maxed")
-                        if maxed:
-                            player.intelligencemax = 1
-                        else:
-                            player.intelligencemax = 0
                     case 3:
                         player.passingscout = 0
                         player.passing = int(only_numerics(player_skill.text))
-                        maxed = player_skill.find(class_="maxed")
-                        if maxed:
-                            player.passingmax = 1
-                        else:
-                            player.passingmax = 0
                     case 4:
                         player.shootingscout = 0
                         player.shooting = int(only_numerics(player_skill.text))
-                        maxed = player_skill.find(class_="maxed")
-                        if maxed:
-                            player.shootingmax = 1
-                        else:
-                            player.shootingmax = 0
                     case 5:
                         player.headingscout = 0
                         player.heading = int(only_numerics(player_skill.text))
-                        maxed = player_skill.find(class_="maxed")
-                        if maxed:
-                            player.headingmax = 1
-                        else:
-                            player.headingmax = 0
                     case 6:
                         player.keepingscout = 0
                         player.keeping = int(only_numerics(player_skill.text))
-                        maxed = player_skill.find(class_="maxed")
-                        if maxed:
-                            player.keepingmax = 1
-                        else:
-                            player.keepingmax = 0
                     case 7:
                         player.controlscout = 0
                         player.control = int(only_numerics(player_skill.text))
-                        maxed = player_skill.find(class_="maxed")
-                        if maxed:
-                            player.controlmax = 1
-                        else:
-                            player.controlmax = 0
                     case 8:
                         player.tacklingscout = 0
                         player.tackling = int(only_numerics(player_skill.text))
-                        maxed = player_skill.find(class_="maxed")
-                        if maxed:
-                            player.tacklingmax = 1
-                        else:
-                            player.tacklingmax = 0
                     case 9:
                         player.aerialscout = 0
                         player.aerial = int(only_numerics(player_skill.text))
-                        maxed = player_skill.find(class_="maxed")
-                        if maxed:
-                            player.aerialmax = 1
-                        else:
-                            player.aerialmax = 0
                     case 10:
                         player.playsscout = 0
                         player.plays = int(only_numerics(player_skill.text))
-                        maxed = player_skill.find(class_="maxed")
-                        if maxed:
-                            player.playsmax = 1
-                        else:
-                            player.playsmax = 0
                     case 11:
                         player.experience = int(only_numerics(player_skill.text))
                     case 12:
                         player.form = int(only_numerics(player_skill.text))
 
                 count += 1
+            
+            #transfer_info = float_right[0].find_all("strong")
+            #player.value = int(only_numerics(money_info[0].text))
+            #player.salary = int(only_numerics(money_info[1].text))
+            
             session.commit()
             players.append(player)
 
-        # Scout Data
-        for player in players:
-            if player.startraining == None:
-                url = (
-                    "https://www.managerzone.com/ajax.php?p=players&sub=scout_report&pid="
-                    + str(player.id)
-                    + "&sport=soccer"
-                )
-                sb.open(url)
-                player = set_player_scout(
-                    scout_page=sb.driver.page_source, player=player
-                )
 
-            session.add(player)
 
-        for player_training in players_training:
-            player_training.trainingdate = utc_string
-            url = (
-                "https://www.managerzone.com/ajax.php?p=trainingGraph&sub=getJsonTrainingHistory&sport=soccer&player_id="
-                + str(player_training.id)
-            )
-            sb.open(url)
-            player_training.trainingdata = format_training_data(sb.driver.page_source)
-            session.add(player_training)
-
-        session.commit()
+    breakpoint
